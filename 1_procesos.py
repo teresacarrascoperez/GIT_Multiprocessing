0c33c043e62afa4cf60231e9bdc7291986695f9d
teresacarrascoperez <terescar@ucm.es>
2022-02-03 20:10:25 +0100

    1_procesos.py

---
+++ 1_procesos.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 11:41:07 2022
+
+@author: teresa
+"""
+
+import time
+import random
+
+from multiprocessing import Process
+
+def f(value):
+    for i in range(3):
+        print (f"hola soy {value} vuelta {i}")
+        time.sleep(random.random()/3)
+
+def g():
+    print ("adios")
+
+if __name__ == "__main__":
+    N = 10
+    lp = []
+    for i in range(N):
+        lp.append(Process(target=f,args=(f"ana {i}",)))
+    for p in lp:
+        p.start()
+    
+    q = Process(target=g)
+    q.start()
+    print ("fin")
