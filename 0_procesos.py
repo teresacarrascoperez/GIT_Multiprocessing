980bc5c61622f6b2594eb0be3f4c3ae7abd965df
terescar@ucm.es <teresa@MacBook-Air-de-Teresa.local>
2022-02-03 20:08:31 +0100

    0_procesos.py

---
+++ 0_procesos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 11:36:12 2022
+
+@author: teresa
+"""
+from time import sleep
+from random import random
+"""
+Documentación multiprocessing
+https://docs.python.org/3.6/library/multiprocessing.html
+"""
+from multiprocessing import Process
+
+def f():
+ for i in range(5):
+     print ("hola", i)
+     sleep(random())
+
+if __name__ == "__main__":
+    p = Process(target=f)
+    q = Process(target=f)
+    p.start()
+    q.start()
+    print ("fin")
